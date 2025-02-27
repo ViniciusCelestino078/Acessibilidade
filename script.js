d14e6de103b5dd5d87d2b55b1b59ee56bfbed765
ViniciusCelestino078 <132959683+ViniciusCelestino078@users.noreply.github.com>
2025-02-27 08:08:20 -0300

    index.htlm

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const alternaContraste = document.getElementById('alterna-contraste')
 const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
 const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 botaoDeAcessibilidade.addEventListener('click', function (){
@@ -5,3 +6,14 @@ botaoDeAcessibilidade.addEventListener('click', function (){
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
    })
    
+   //Código omitido
+
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+
+//Código omitido
+
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
